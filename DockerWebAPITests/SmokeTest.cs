40af75f28b286c4902a0c37c8d0535c822442b3f
Janus <januspeis@gmail.com>
2023-02-11 20:58:13 +0100

    init

---
+++ DockerWebAPITests/SmokeTest.cs
@@ -0,0 +1,23 @@
+﻿using DockerWebAPI.Pulumi;
+using DockerWebAPI.Pulumi.InfrastructureTemplates;
+using NUnit.Framework;
+
+namespace DockerWebAPITests;
+
+public class SmokeTest
+{
+    [Test]
+    public void Smoke([Values]CpuSize cpus)
+    {
+        Console.WriteLine(cpus.ToCpuUnits());
+    }
+    
+    [Test]
+    [Ignore("just checking stuff")]
+    public void Sasdmoke([Values]CpuSize cpus)
+    {
+        new auctionsdk.Client(null, null).GetWeatherForecastAsync(null);
+    }
+    
+    
+}
--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# send-grid-poc
+## Define github secrets:
+
+AWS_ACCESS_KEY\
+AWS_REGION\
+AWS_SECRET_ACCESS_KEY\
+PULUMI_ACCESS_TOKEN
+
+inorder to get swagger tool to work, you need to include Swashbuckle.AspNetCore.Annotations ....
+https://khalidabuhakmeh.com/generate-aspnet-core-openapi-spec-at-build-time
